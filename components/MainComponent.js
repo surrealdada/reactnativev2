854ad3991121cd6dba10c27b89b200d1d550ccb2
Karl Suits <karlsuits@gmail.com>
2022-04-21 10:50:41 -0400

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Reservation from './ReservationComponent';
 import Constants from "expo-constants";
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
@@ -92,6 +93,8 @@ const ContactNavigator = createStackNavigator(
   }
 );
 
+
+
 const CustomDrawerContentComponent = props => (
   <ScrollView>
       <SafeAreaView 
@@ -108,6 +111,29 @@ const CustomDrawerContentComponent = props => (
           <DrawerItems {...props} />
       </SafeAreaView>
   </ScrollView>
+);
+
+const ReservationNavigator = createStackNavigator(
+  {
+      Reservation: { screen: Reservation }
+  },
+  {
+      defaultNavigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='tree'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
 );
 
 const MainNavigator = createDrawerNavigator(
@@ -137,8 +163,22 @@ const MainNavigator = createDrawerNavigator(
                   />
               )
           }
-      },
-      About: {
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        About: {
           screen: AboutNavigator,
           navigationOptions: {
               drawerLabel: 'About Us',
